f5581ac7039b3dbfb4c2449e69e09a00aba18ffe
Maurice <maurice.green0740@gmail.com>
2024-08-27 18:12:27 -0400

    add script

--- scripts/build/quick_compile_macos.sh
+++ scripts/build/quick_compile_macos.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# test script 
 nasm -f macho64 src/cpuidIsSupported.asm -o build/cpuidIsSupported.o
 
 gcc -m64 main.c src/cpu_interface.c build/cpuidIsSupported.o -o build/bin/cpu_analyzer
